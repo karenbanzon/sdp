c2a11ffe8f4a4db8276c8de42b1e802a0bba415d
Karen Banzon <kabanzon@gmail.com>
2013-08-26 19:21:55 +0800

    Added base styles

---
+++ web/static/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="shortcut icon" href="/images/icons/favicon/favicon.ico" />
+		<!-- Load Stylesheet -->
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
+
+		<title>UP Sigma Delta Phi</title>
+	</head>
+
+	<body>
+		<header class="siteHead">
+			<div id="brand">
+				<div>
+					<span class="icon iconseal" aria-hidden="true"></span>
+				</div>
+			</div>
+			<nav>
+				<a href="">About Us</a>
+				<a href="">Project Bata, Mag-aral Ka</a>
+				<a href="">UP Powercard</a>
+				<a href="">Contact Us</a>
+			</nav>
+		</header>
+
+		<section class="main">
+			Content here
+		</section>
+
+		<footer class="siteFoot">
+			Copyright &copy; 2013 UP Sigma Delta Phi. All Rights Reserved.
+		</footer>
+	</body>
+</html>
