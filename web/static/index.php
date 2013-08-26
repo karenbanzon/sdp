062c0a1c5fd5370a7c900eedb073e9e460dbe215
Karen Banzon <kabanzon@gmail.com>
2013-08-27 00:14:33 +0800

    Added new styles, added form page

--- web/static/index.php
+++ web/static/index.php
@@ -1,46 +1,14 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="shortcut icon" href="/images/icons/favicon/favicon.ico" />
-		<!-- Load Stylesheet -->
-		<link rel="stylesheet" type="text/css" href="/css/style.css">
-
-		<title>UP Sigma Delta Phi</title>
-	</head>
-
-	<body>
-		<header class="siteHead">
-			<div id="brand">
-				<div>
-					<span class="icon iconseal" aria-hidden="true"></span>
-					<div class="social">
-						<span class="icon iconfacebook" aria-hidden="true"></span>
-						<span class="icon icontwitter" aria-hidden="true"></span>
-						<span class="icon iconpinterest" aria-hidden="true"></span>
-					</div>		
-				</div>
-			</div>
-			<nav>
-				<a href="">About Us</a>
-				<a href="">Project Bata, Mag-aral Ka</a>
-				<a href="">UP Powercard</a>
-				<a href="">Contact Us</a>
-			</nav>
-		</header>
+<?php require 'includes/header.php' ?>
 
 		<section class="main">
 			<div class="content" id="homepage">
 				<div class="primaryBlock">
 					<div class="video">
-						<iframe width="800" height="600" src="//www.youtube.com/embed/zAvDiVFTOnk" frameborder="0" allowfullscreen></iframe>
+						<iframe src="https://www.facebook.com/video/embed?video_id=10151351924767143" width="800" height="600" frameborder="0"></iframe>
 					</div>
-					<a href="" class="button">Membership Qualifications <span class="icon iconarrowright"></span></a>
+					<a href="form.php" class="button">Membership Qualifications <span class="icon iconarrowright"></span></a>
 				</div>
 			</div>
 		</section>
 
-		<footer class="siteFoot">
-			Copyright &copy; 2013 UP Sigma Delta Phi. All Rights Reserved.
-		</footer>
-	</body>
-</html>
+<?php require 'includes/footer.php' ?>
