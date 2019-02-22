cefa42e21b4e3a6cd4a211168628df8b9d19bf51
Karen Banzon <karen.banzon@olx.com>
2019-02-23 00:10:55 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -91,5 +91,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
+	define( 'UPLOADS', 'wp-content/uploads' );
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
